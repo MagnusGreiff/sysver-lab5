0dd2e137ef675482ffb33a1115bdb7d1645ccb98
Magnus Greiff <mangegreiff@gmail.com>
2019-05-19 21:12:10 +0200

    Implementation of user story 1.

--- src/Frame.java
+++ src/Frame.java
@@ -1,2 +1,24 @@
 public class Frame {
+
+    private int[] score;
+    private int currentThrow;
+
+
+    public Frame() {
+        this.score = new int[2];
+        this.currentThrow = 0;
+    }
+
+    public void setThrow(int i) {
+        this.score[currentThrow] = i;
+        this.currentThrow++;
+    }
+
+    public int getThrowOne() {
+        return this.score[0];
+    }
+
+    public int getThrowTwo() {
+        return this.score[1];
+    }
 }
