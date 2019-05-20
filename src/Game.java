72ae8d170c5c2c278af6e570c644b1d5fb706100
Magnus Greiff <mangegreiff@gmail.com>
2019-05-20 20:23:19 +0200

    Implementation of user story 4.

--- src/Game.java
+++ src/Game.java
@@ -1,11 +1,33 @@
+import java.util.Arrays;
+
 class Game {
     private Frame[] frames;
 
     Game() {
         this.frames = new Frame[10];
+
+        Arrays.setAll(this.frames, i -> new Frame());
     }
 
     Frame[] allFrames() {
         return this.frames;
     }
+
+    void setScore(int[] score) {
+        int count = 0;
+        for (Frame frame : this.frames) {
+            frame.setThrow(score[count]);
+            frame.setThrow(score[count + 1]);
+            count += 2;
+        }
+    }
+
+    int getScore() {
+        int score = 0;
+        for (Frame frame: this.frames) {
+            score += frame.getThrowScore();
+        }
+        return score;
+    }
 }
+// System.out.print()
