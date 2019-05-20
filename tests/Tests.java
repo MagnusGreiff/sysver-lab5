ba24641869d4909def5c7c9d3da144e3f38c37fd
Magnus Greiff <mangegreiff@gmail.com>
2019-05-20 21:39:53 +0200

    Test case for user story 9.

--- tests/Tests.java
+++ tests/Tests.java
@@ -86,4 +86,14 @@ public class Tests {
         int res = game.getScore();
         assertEquals(112, res);
     }
+
+
+    @Test
+    public void MultipleSpares() {
+        Game game = new Game();
+        int[] score = {8, 2, 5, 5, 7, 2, 3, 6, 4, 4, 5, 3, 3, 3, 4, 5, 8, 1, 2, 6};
+        game.setScore(score);
+        int res = game.getScore();
+        assertEquals(98, res);
+    }
 }
